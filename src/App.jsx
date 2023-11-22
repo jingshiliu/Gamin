65d50b90be325bd6f119f0c83f82b35334583b5f
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-22 18:54:47 -0500

    chore(App): create components

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,39 @@
 import { useState } from 'react'
 
+import {CustomerReviews, Footer, Hero, Services, Subscribe, SpecialOffer, PopularProducts, SuperQuality} from './sections'
+import Nav from './components/Nav'
+
 function App() {
 
 
   return (
-    <>
-
-    </>
+    <main className='relative text-slate-700'>
+        <Nav/>
+        <section className="xl:p-10 p-10">
+            <Hero/>
+        </section>
+        <section className="p-10">
+            <PopularProducts/>
+        </section>
+        <section className="p-10">
+            <SuperQuality/>
+        </section>
+        <section className="p-10">
+            <Services/>
+        </section>
+        <section className="p-10">
+            <SpecialOffer/>
+        </section>
+        <section className="p-10">
+            <CustomerReviews/>
+        </section>
+        <section className="p-10">
+            <Subscribe/>
+        </section>
+        <footer className="p-10 bg-slate-500">
+            <Footer/>
+        </footer>
+    </main>
   )
 }
 
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import CustomerReviews from "./CustomerReviews.jsx";
+import Hero from "./Hero.jsx";
+import Footer from "./Footer.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Subscribe from "./Subscribe.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+import PopularProducts from "./PopularProducts.jsx";
+
+export {
+    CustomerReviews,
+    Hero,
+    Footer,
+    Services,
+    SpecialOffer,
+    Subscribe,
+    SuperQuality,
+    PopularProducts
+}
