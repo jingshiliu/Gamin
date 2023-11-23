cbca9a3f9691277b57aceec43ca72abc8e2a1c5b
Jingshi Liu <jingshiliu07@gmail.com>
2023-11-22 20:08:00 -0500

    feat(Nav): renaming brand to Gamin with new Logo

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,12 +1,17 @@
-import NikeLogo from '../assets/images/header-logo.svg';
+import { PiGameController } from "react-icons/pi";
 import Hamburguer from '../assets/icons/hamburger.svg';
 import {useState} from "react";
 
 const Nav = () => {
     const [showMenu, setShowMenu] = useState(false);
     return (
-        <header className="xl:px-36 lg:px-20 px-12 p-10  flex justify-between max-md:text-blue-600 relative">
-            <h1><img src={NikeLogo} alt="Nike Logo"/></h1>
+        <header className="xl:px-36 lg:px-20 px-12 p-10  flex justify-between items-center relative">
+            <h1 className={'flex items-center'}>
+                <PiGameController className={'w-10 h-10'}/>
+                <span className={'text-lg font-mono ml-2'}>
+                    Gamin
+                </span>
+            </h1>
             <nav
                 className={'text-base flex justify-between lg:space-x-14 text-slate-500 font-light max-md:hidden md:space-x-8 items-center'}>
                 <a href="">Home</a>
@@ -25,7 +30,7 @@ const Nav = () => {
 
             {showMenu ? (
                 <nav
-                    className={'text-base flex flex-col justify-between text-slate-500 font-light md:hidden items-center absolute left-0 border-b-2 w-full mt-14 space-y-5 text-center'}
+                    className={'text-base flex flex-col justify-between text-slate-500 font-light md:hidden items-center absolute left-0 top-2/3 border-b-2 w-full space-y-5 text-center'}
                 >
                     <a href="">Home</a>
                     <a className={'w-1/3 border-t-2 border-slate-200 pt-4 hover:text-slate-400'} href="">About Us</a>
